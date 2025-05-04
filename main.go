2a9bd86345d1b0b33feeabd8ac0a47e561133aaa
Jeremy Adams <jeremy@dagger.io>
2025-05-04 13:10:31 -0700

    first
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

---
+++ main.go
@@ -0,0 +1,26 @@
+// MayTheFourth will turn your webpage into a Jedi koan
+
+package main
+
+import (
+	"context"
+)
+
+type MayTheFourth struct{}
+
+func (m *MayTheFourth) Run(ctx context.Context, url string) (string, error) {
+	summary, err := dag.TechSummarizerAgent().Summarize(ctx, url)
+	if err != nil {
+		return "", err
+	}
+	environment := dag.Env().
+		WithStringInput("summary", summary, "short page summary").
+		WithStringOutput("koan", "a short Jedi koan based on $summary")
+	return dag.LLM().
+		WithEnv(environment).
+		WithPrompt("You're a Jedi master, and a Zen master. Write accordingly.").
+		Loop().
+		Env().
+		Output("koan").
+		AsString(ctx)
+}
